7b8c439a1359dca622aa2ba11733b4119d8a8fa5
onlybond <36098799+onlybond@users.noreply.github.com>
2023-08-11 16:11:17 +0530

    added animations using gsap

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-import {API_KEY} from './config.js'
+import { API_KEY } from './config.js'
 document.addEventListener("DOMContentLoaded", () => {
+    const animatediv = document.querySelectorAll("div")
+    const animateserchdiv = document.querySelectorAll("div:not(header)");
     const locationElement = document.querySelector(".location-and-date__location");
     const locationDate = document.querySelector(".location-and-date__location-date");
     const currentTemperatureElement = document.querySelector(".current-temperature__value");
@@ -14,16 +16,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Replace with your API key
     const apiKey = API_KEY;
-    console.log(apiKey);
     const apiUrl = "https://api.openweathermap.org/data/2.5/";
 
+    function showScrollMessage() {
+        const scrollMessage = document.querySelector('.scroll-message');
+
+        const timeline = gsap.timeline({
+            defaults: { duration: 1 },
+            onComplete: () => {
+                // Hide the scroll message when the animation is complete
+                gsap.set(scrollMessage, { opacity: 0 });
+            },
+        });
+
+        timeline.to(scrollMessage, { opacity: 0 });
+
+        ScrollTrigger.create({
+            trigger: scrollMessage,
+            start: 'top center',
+            animation: timeline,
+            toggleActions: 'play none none',
+        });
+    }
+
+    // Call the function to show the scroll message
+    showScrollMessage();
+    function animateFadeIn(elements) {
+        if (!elements || elements.length === 0) {
+            console.error("No elements provided for animation.");
+            return;
+        }
+
+        gsap.from(elements, {
+            opacity: 0,
+            y: 40,
+            duration: 1,
+            stagger: 0.1,
+            ease: "power4.out",
+        });
+    }
     function getWeatherData(latitude, longitude) {
         const weatherUrl = `${apiUrl}weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
         fetch(weatherUrl)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 const locationdate = new Date(data.dt * 1000);
                 const temperature = data.main.temp;
                 const weatherCondition = data.weather[0].description;
@@ -41,6 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 lowTemperatureElement.textContent = `${Math.round(lowTemperature)}°C`;
                 windSpeedElement.textContent = `${windSpeed} m/s`;
                 humidityElement.textContent = `${humidity}%`;
+                animateFadeIn([animateserchdiv]);
             })
             .catch(error => {
                 console.error("Error fetching weather data:", error);
@@ -54,16 +92,20 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(response => response.json())
             .then(data => {
                 const forecastItems = data.list;
-                // Group forecast items by date
+
+                // Group forecast items by date excluding today
                 const groupedForecast = {};
                 forecastItems.forEach(item => {
                     const date = new Date(item.dt * 1000).toLocaleDateString("en-US", { weekday: "short", month: "numeric", day: "numeric" });
 
-                    if (!groupedForecast[date]) {
-                        groupedForecast[date] = [];
-                    }
+                    // Exclude today's forecast items
+                    if (date !== new Date().toLocaleDateString("en-US", { weekday: "short", month: "numeric", day: "numeric" })) {
+                        if (!groupedForecast[date]) {
+                            groupedForecast[date] = [];
+                        }
 
-                    groupedForecast[date].push(item);
+                        groupedForecast[date].push(item);
+                    }
                 });
 
                 // Clear previous forecast data
@@ -73,6 +115,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 for (const date in groupedForecast) {
                     const forecastRow = document.createElement("div");
                     forecastRow.classList.add("next-5-days__row");
+                    forecastRow.setAttribute("data-date", date);
                     const firstItem = groupedForecast[date][0];
                     const itemday = new Date(firstItem.dt * 1000).toLocaleDateString("en-US", { weekday: "short" });
                     const itemdate = new Date(firstItem.dt * 1000).toLocaleDateString("en-US", { month: "numeric", day: "numeric" });
@@ -90,8 +133,85 @@ document.addEventListener("DOMContentLoaded", () => {
                     <div class="next-5-days__icon"><img src="icons/${weatherIcon}.svg" alt=""><div class="next-5-days__label">${weatherCondition}</div></div>
                     <div class="next-5-days__rain">${Math.round(rainPercentage * 100)}%<div class="next-5-days__label">Rain</div></div>
                     <div class="next-5-days__wind">${windSpeed} m/s<div class="next-5-days__label">Wind</div></div>
+                    <div class="next-5-days__details">
+                        <div class="forecast-details__dropdown">
+                            <!-- Hourly forecast details will be added here -->
+                        </div>
+                    </div
                 `;
+
+                    forecastRow.addEventListener("click", () => {
+                        // Toggle active class on the clicked forecast row
+                        forecastRow.classList.toggle("active");
+
+                        // Toggle visibility of the forecast details dropdown
+                        const forecastDetailsDropdown = forecastRow.querySelector(".forecast-details__dropdown");
+                        if (forecastDetailsDropdown) {
+                            forecastDetailsDropdown.classList.toggle("active");
+
+                            // Fetch and populate hourly forecast data if the dropdown is active
+                            if (forecastDetailsDropdown.classList.contains("active")) {
+                                const forecastDate = forecastRow.getAttribute("data-date"); // Use data-date attribute
+
+                                if (forecastDate) {
+                                    const hourlyForecast = groupedForecast[forecastDate];
+
+                                    if (hourlyForecast) {
+                                        // Clear previous hourly forecast data
+                                        forecastDetailsDropdown.innerHTML = "";
+                                        console.log(hourlyForecast);
+                                        // Populate hourly forecast data
+                                        hourlyForecast.forEach(item => {
+                                            const timeOptions = { hour: "numeric", hour12: true };
+                                            const time = new Date(item.dt * 1000).toLocaleTimeString("en-US", timeOptions);
+                                            const temperatureHigh = item.main.temp_max;
+                                            const weatherCondition = item.weather[0].description
+                                            const temperatureLow = item.main.temp_min;
+                                            const weatherIcon = item.weather[0].icon;
+                                            const rainPercentage = item.pop;
+                                            const windSpeed = item.wind.speed;
+
+                                            const hourlyForecastRow = document.createElement("div");
+                                            hourlyForecastRow.classList.add("hourly-forecast__row");
+                                            hourlyForecastRow.innerHTML = `
+                                            <div class="hourly-forecast__time">${time}</div>
+                                            <div class="hourly-forecast__icon"><img src="icons/${weatherIcon}.svg" alt=""></div>
+                                            <div class="hourly-forecast__weatherCondition">${weatherCondition}</div>
+                                            <div class="hourly-forecast__temperatureHigh">${temperatureHigh}°C</div>
+                                            <div class="hourly-forecast__temperatureLow">${temperatureLow}°C</div>
+                                            <div class="hourly-forecast__Humidity">${rainPercentage}%</div>
+                                            <div class="hourly-forecast__windSpeed">${windSpeed} m/s</div>
+                                        `;
+
+                                            forecastDetailsDropdown.appendChild(hourlyForecastRow);
+                                        });
+                                    } else {
+                                        console.error("Hourly forecast data not found for the selected date.");
+                                    }
+                                } else {
+                                    console.error("Forecast date not found in data-date attribute.");
+                                }
+                            }
+                        }
+                    });
+
+
+
+
                     next5DaysContainer.appendChild(forecastRow);
+                    gsap.to(forecastRow, { opacity: 0, y: 100, duration: 0 });
+
+                    // Use ScrollTrigger for viewport-based animation
+                    gsap.to(forecastRow, {
+                        opacity: 1,
+                        y: 0,
+                        duration: 1,
+                        ease: "power4.out",
+                        scrollTrigger: {
+                            trigger: forecastRow,
+                            start: "top 80%", // Adjust the starting point as needed
+                        },
+                    });
                 }
             })
             .catch(error => {
@@ -99,60 +219,33 @@ document.addEventListener("DOMContentLoaded", () => {
             });
     }
 
+
+    const searchButton = document.querySelector(".search-btn");
     const searchInput = document.getElementById("location-input");
     const suggestionsContainer = document.querySelector(".suggestions");
 
-    let typingTimer;
-    const typingDelay = 500; // Delay in milliseconds
-
-    searchInput.addEventListener("input", () => {
-        clearTimeout(typingTimer);
-
-        const inputValue = searchInput.value;
+    searchButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const inputValue = searchInput.value
         if (inputValue) {
-            typingTimer = setTimeout(() => {
-                fetchSuggestions(inputValue);
-            }, typingDelay);
-        } else {
-            suggestionsContainer.innerHTML = "";
-        }
-    });
+            suggestionsContainer.innerHTML = ''
+            const searchUrl = `${apiUrl}weather?q=${inputValue}&appid=${apiKey}`;
+            fetch(searchUrl)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.cod == 200) {
 
-    searchInput.addEventListener("keydown", event => {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            const inputValue = searchInput.value;
-            if (inputValue) {
-                fetchSuggestions(inputValue);
-            }
-        }
-    });
-
-    function fetchSuggestions(inputValue) {
-        const searchUrl = `${apiUrl}weather?q=${inputValue}&appid=${apiKey}`;
-        fetch(searchUrl)
-            .then(response => response.json())
-            .then(data => {
-                suggestionsContainer.innerHTML = "";
-
-                if (data.name) {
-                    const suggestion = document.createElement("div");
-                    suggestion.classList.add("suggestion");
-                    suggestion.textContent = data.name;
-                    suggestion.addEventListener("click", () => {
-                        searchInput.value = data.name;
-                        suggestionsContainer.innerHTML = "";
                         const latitude = data.coord.lat;
                         const longitude = data.coord.lon;
                         getWeatherAndForecast(latitude, longitude);
-                    });
-                    suggestionsContainer.appendChild(suggestion);
-                }
-            })
-            .catch(error => {
-                console.error("Error fetching suggestions:", error);
-            });
-    }
+                    }
+                    else if (data.cod == 404) {
+                        suggestionsContainer.innerHTML = "city not found";
+                    }
+                    showScrollMessage();
+                })
+        }
+    })
     function getWeatherAndForecast(latitude, longitude) {
         getWeatherData(latitude, longitude);
         getForecastData(latitude, longitude);
